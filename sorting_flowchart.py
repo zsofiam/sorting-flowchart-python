30512754a4b8b18df9c5c35f7fc604779276d9af
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-02 16:32:28 +0100

    sorting algorithm implemented

--- sorting_flowchart.py
+++ sorting_flowchart.py
@@ -1,1 +1,17 @@
-# write your code here
+def main():
+    print(bubble_sort([4, 5, 2, 1, 0, 9]))
+
+
+def bubble_sort(numbers):
+    N = len(numbers)
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if (numbers[j] > numbers[j+1]):
+                temp = numbers[j+1]
+                numbers[j+1] = numbers[j]
+                numbers[j] = temp
+    return numbers
+    
+    
+if __name__ == '__main__':
+    main()
